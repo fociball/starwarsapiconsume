efbc1e362775c9bf65a0ed9631cbb43d0f0d9ed1
Nicholas <wampamagi@gmail.com>
2019-10-08 19:53:53 -0400

    Fleshed out People model

--- src/main/java/model/People.java
+++ src/main/java/model/People.java
@@ -13,6 +13,36 @@ public class People {
 
     @JsonProperty
     String name;
+    @JsonProperty
+    int height;
+    @JsonProperty
+    int mass;
+    @JsonProperty(value = "hair_color")
+    String hairColor;
+    @JsonProperty(value = "skin_color")
+    String skinColor;
+    @JsonProperty(value = "eye_color")
+    String eyeColor;
+    @JsonProperty(value = "birth_year")
+    String birthYear;
+    @JsonProperty
+    String gender;
+    @JsonProperty
+    String homeworld;
+    @JsonProperty
+    String[] films;
+    @JsonProperty
+    String[] species;
+    @JsonProperty
+    String[] vehicles;
+    @JsonProperty
+    String[] starships;
+    @JsonProperty
+    String created;
+    @JsonProperty
+    String edited;
+    @JsonProperty
+    String url;
 
     public People(String name){
         this.name = name;
